fe586bba6722110ff9cf2837aa329de13e3a39b4
RuhulAmin3 <dev.ruhulamin3@gmail.com>
2025-06-25 22:06:22 +0600

    marge and fix auth issue

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,16 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application, ErrorRequestHandler, Response } from 'express';
 import httpStatus from 'http-status';
-import morgan from 'morgan';
+// import morgan from 'morgan';
 import path from 'path';
 import GlobalErrorHandler from './app/middlewares/globalErrorHandler';
 import { defaultLimiter } from './app/middlewares/rateLimit';
 import router from './app/routes';
-import logger from './app/utils/logger';
+// import logger from './app/utils/logger';
 import { restrictToNordVPN } from './app/middlewares/restrictToNordVPN';
 
 const app: Application = express();
-const morganFormat = ':method :url :status :response-time ms';
+// const morganFormat = ':method :url :status :response-time ms';
 
 const corsOptions = {
   origin: ['*'],
@@ -34,23 +34,23 @@ router.use(restrictToNordVPN());
 // 📂 Static files
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
-// 📋 Logging
-app.use(morgan('dev'));
-app.use(
-  morgan(morganFormat, {
-    stream: {
-      write: (message) => {
-        const logObject = {
-          method: message.split(' ')[0],
-          url: message.split(' ')[1],
-          status: message.split(' ')[2],
-          responseTime: message.split(' ')[3],
-        };
-        logger.info(JSON.stringify(logObject));
-      },
-    },
-  }),
-);
+// // 📋 Logging
+// app.use(morgan('dev'));
+// app.use(
+//   morgan(morganFormat, {
+//     stream: {
+//       write: (message) => {
+//         const logObject = {
+//           method: message.split(' ')[0],
+//           url: message.split(' ')[1],
+//           status: message.split(' ')[2],
+//           responseTime: message.split(' ')[3],
+//         };
+//         logger.info(JSON.stringify(logObject));
+//       },
+//     },
+//   }),
+// );
 
 // 🌐 Base route
 app.get('/', (_req, res: Response) => {
